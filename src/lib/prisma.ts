d26a65e2987599375a649c6638cc7e1818cfeb85
Harsh Pal <Harsh.Pal@wisetechglobal.com>
2025-12-03 15:11:04 +0530

    Changes in the db configuration

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -9,3 +9,10 @@ export const prisma = globalForPrisma.prisma ?? new PrismaClient({
 })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+// Graceful shutdown
+if (process.env.NODE_ENV === 'production') {
+  process.on('beforeExit', async () => {
+    await prisma.$disconnect()
+  })
+}
